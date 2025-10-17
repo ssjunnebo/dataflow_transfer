b5ca98228b5fed04b84d03eff13f4bdbbe76b2f6
Sara Sjunnebo <sara.sjunnebo@gmail.com>
2025-10-17 08:20:04 +0200

    Wrap registry in a class

--- dataflow_transfer/run_classes/registry.py
+++ dataflow_transfer/run_classes/registry.py
@@ -1,8 +1,29 @@
-RUN_CLASS_REGISTRY = {}
+from types import MappingProxyType
 
 
-def register_run_class(cls):
-    run_type = getattr(cls, "run_type", None)
-    if run_type:
-        RUN_CLASS_REGISTRY[run_type] = cls
-    return cls
+class RunClassRegistry:
+    _registry = {}
+
+    @classmethod
+    def register(cls, run_cls):
+        run_type = getattr(run_cls, "run_type", None)
+        if run_type:
+            cls._registry[run_type] = run_cls
+        return run_cls
+
+    @classmethod
+    def get(cls, run_type):
+        return cls._registry.get(run_type)
+
+    @classmethod
+    def view(cls):
+        return MappingProxyType(cls._registry)
+
+
+# Decorator for registering run classes
+def register_run_class(run_cls):
+    return RunClassRegistry.register(run_cls)
+
+
+# Immutable view for external use
+RUN_CLASS_REGISTRY = RunClassRegistry.view()
